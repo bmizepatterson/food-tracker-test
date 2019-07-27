5059946c1fda61db67af70a9c1e0fd82d0e72645
Ben Patterson <bmizepatterson@gmail.com>
2019-07-27 11:35:12 -0400

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Ben Patterson on 7/27/19.
+//  Copyright © 2019 Ben Patterson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
